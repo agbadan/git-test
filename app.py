179276e172de4fe35e878ed14e1cac21d16749d7
agbadan <64725293+agbadan@users.noreply.github.com>
2025-03-06 22:33:28 +0000

    Update app.py
    //Changement test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello, Flask!</p>"
+    return "<p>Hello, Flask! Hi It is a modif</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
